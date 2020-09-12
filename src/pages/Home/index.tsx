4d44c02788988746b6931db5ae6f380a1687ece5
Daniel <danielsvba@hotmail.com>
2020-09-12 20:59:06 -0300

    feature(app): Ícones e splash screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
 import { View, Text } from 'react-native';
 
 function Home() {
+
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return (
         <View>
             <Text>
